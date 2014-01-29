wadqc/WAD_AnalysisMatlab

commit f0f3c2971c6dd3f81091e429545e33e48cbcba08

Added: read Toshiba B0 maps (iAAS)

WAD/WAD_MR/WAD_MR_B0_uniformity.m
@@ -125,6 +125,22 @@ if isempty( magnitude ) || isempty( phase )
     return
 end
 
+% check type of data, options are:
+% - dPhi_rad: (wrapped) phase in radians [default]
+% - dB0_ppm : B0 in ppm
+if ~isfield( phase, 'type' ) || isempty( phase.type )
+    % apply default
+    phase.type = 'dPhi_rad';
+end
+
+% consistency check
+if ~isfield( phase, phase.type )
+    % image data not consistent with give data type
+    WAD_vbprint( [my.name ': consistency check failed! Phase type ' phase.type ' but field phase.' phase.type ' doesn''t exist.' ], 1 );
+    return
+end
+
+
 % update waitbar
 if isInteractive, waitbar( 0.2, h ); end
 
@@ -147,9 +163,9 @@ if isInteractive, waitbar( 0.5, h ); end
 axis_x = floor( szROI * diameter_pix(1) / 2 );
 axis_y = floor( szROI * diameter_pix(2) / 2 );
 
-[phase.masked, mask] = ROImask( axis_x, axis_y, centre_pix, phase.dPhi_rad, 0 );
+[phase.masked, mask] = ROImask( axis_x, axis_y, centre_pix, phase.(phase.type), 0 );
 
-magnitude.masked = immultiply( magnitude.image, mask);
+magnitude.masked = magnitude.image .* mask;
 
 % ----------------------------------------------------
 % haalt bovenste stuk fantoom af omdat daar afwijkende geometrie,
@@ -160,7 +176,7 @@ phasemask(:,:) = mask(:,:);
 ignoreTopRows = floor( centre_pix(2) - ignoreTop*axis_x );
 phasemask( 1:ignoreTopRows, : ) = 0;
 
-phase.masked = immultiply( phase.masked, phasemask );
+phase.masked = phase.masked .* phasemask;
 
 % update waitbar
 if isInteractive, waitbar( 0.6, h ); end
@@ -173,24 +189,29 @@ if isInteractive, waitbar( 0.6, h ); end
 x_pix=floor(centre_pix(1));
 y_pix=floor(centre_pix(2));
 
-phase.unwrapped = unwrap2D( phase.masked, [x_pix,y_pix]);
-phase.maskedandunwrapped = immultiply(phase.unwrapped, phasemask);
+% unwrapping fasehoek, en conversie naar B0
+if strcmp( phase.type, 'dPhi_rad' )
+    phase.unwrapped = unwrap2D( phase.masked, [x_pix,y_pix]);
+    phase.maskedandunwrapped = phase.unwrapped .* phasemask;
+    % update waitbar
+    if isInteractive, waitbar( 0.8, h ); end
 
-% update waitbar
-if isInteractive, waitbar( 0.8, h ); end
+    % ----------------------------------------------------
+    % bereken B0
+    % ----------------------------------------------------
+    gamma = 267513; %42576;  % gyromatric frequency in rad/s*1/T
+    magnet_T = phase.info.MagneticFieldStrength;  % in Tesla
 
-% ----------------------------------------------------
-% bereken B0
-% ----------------------------------------------------
-gamma = 267513; %42576;  % gyromatric frequency in rad/s*1/T
-magnet_T = phase.info.MagneticFieldStrength;  % in Tesla
-
-dB0_T = phase.maskedandunwrapped / (gamma .* phase.dTE); % in Tesla
+    dB0_T = phase.maskedandunwrapped / (gamma .* phase.dTE); % in Tesla
+    dB0_ppm = dB0_T / magnet_T * 1000000; % in ppm
+else
+    % For B0 map: just apply the mask
+    dB0_ppm = phase.dB0_ppm .* phasemask;
+end
 
 % write B0 map to calculations log file
 hFig = figure( 'Visible', fig_visible, 'MenuBar', 'none', 'Name', 'B0 map [ppm]' );
-%hFig = figure( 'Name', 'B0 map [ppm]' );
-imshow( dB0_T / magnet_T * 1000000, [] ); % in ppm
+imshow( dB0_ppm, [] ); % in ppm
 colormap(jet);
 axis image
 axis square
@@ -212,19 +233,19 @@ if isInteractive, waitbar( 0.9, h ); end
 % ----------------------------------------------------
 % bereken uniformiteit
 % ----------------------------------------------------
-matrixsize_phase = size( phase.maskedandunwrapped );
+matrixsize_phase = size( phase.dB0_ppm );
 
 smallest = +1.0E99; % huge
 largest  = -1.0E99; % negative huge
 
 for i=1:matrixsize_phase(1)
     for j=1:matrixsize_phase(2)
-        if phasemask(i,j)==1
-            if dB0_T(i,j)<smallest
-                smallest=dB0_T(i,j);
+        if phasemask(i,j) == 1
+            if phase.dB0_ppm(i,j) < smallest
+                smallest = phase.dB0_ppm(i,j);
             end
-            if dB0_T(i,j)>largest
-                largest=dB0_T(i,j);
+            if phase.dB0_ppm(i,j) > largest
+                largest = phase.dB0_ppm(i,j);
             end
         end
     end
@@ -237,7 +258,7 @@ if isInteractive, waitbar( 1.0, h ); end
 % ----------------------------------------------------
 % final result: difference in ppm
 % ----------------------------------------------------
-B0_uniformity_ppm = ( largest-smallest ) / magnet_T * 1000000; % in ppm
+B0_uniformity_ppm = largest - smallest; % in ppm
 WAD_resultsAppendFloat( 1, B0_uniformity_ppm, 'Uniformity', 'ppm', 'B0' );
 
 % log file
@@ -248,27 +269,22 @@ WAD_vbprint( [my.name ':   B0 uniformity = ' num2str(B0_uniformity_ppm) ' ppm'] 
 if isInteractive, close( h ), end
 
 return
+end
 
 
 
-
-
-% -------------------------------------------------------------------------
 function [B,M] = ROImask(a,b,cent,I,valfill)
+% -------------------------------------------------------------------------
 % Creates an elliptical mask defined by paramaters a, b and cent. Multiples
 % this mask to I and fills all pixels outside ellipse with valfill
 % -------------------------------------------------------------------------
 centx = cent(1);
 centy = cent(2);
-[numr,numc]=size(I);
-
-[x,y] = meshgrid(1:numc,1:numr);
-M = double(((x-centx).^2)/a^2 + ((y-centy).^2)/b^2 <= 1);
-% figure
-% imagesc(M)
-B = immultiply(I,M) + imcomplement(M)*valfill;
-
-
+[numr,numc] = size(I);
+[x,y] = meshgrid( 1:numc, 1:numr );
+M = double( ((x-centx).^2) ./ a.^2 + ((y-centy).^2) ./ b.^2 <= 1 );
+B = M .* I + ( 1.0 - M ) .* valfill;
+end
 
 
 
@@ -305,6 +321,8 @@ for count=1:size_image(1)
     image_out=unwrap_line(image_out(:,:),count,y,size_image,2);
 end
 
+end
+
 
 
 
@@ -321,3 +339,4 @@ result_start = unwrap( row_start, [], orientation);
 % ge-unwrapte delen weer invoegen in uiteindelijke image:
 image_out(x, y:size_image(2))  = result_end( :,: );
 image_out(x, 1:y-1          )  = fliplr( result_start( 1, 2:y ) );
+end

WAD/WAD_MR/WAD_MR.m
@@ -30,6 +30,9 @@ function WAD_MR( wadInputFileXML )
 % 20131127 / JK / v1.1
 % Support new (v1.1) style action limits
 % ------------------------------------------------------------------------
+% 20140116 / JK / v1.1.1
+% - Support Toshiba B0 map (iAAS)
+% ------------------------------------------------------------------------
 
 
 % ----------------------
@@ -37,8 +40,8 @@ function WAD_MR( wadInputFileXML )
 % ----------------------
 % version info
 my.name = 'WAD_MR';
-my.version = '1.1';
-my.date = '20131127';
+my.version = '1.1.1';
+my.date = '20140116';
 
 disp( ['Starting analysis module ' my.name '  Version ' my.version ' ' my.date] );
 

WAD/WAD_MR/WAD_MR_B0_readToshibaAAS.m
@@ -1,0 +1,153 @@
+% ------------------------------------------------------------------------
+% WAD_MR is an MRI analysis module written for IQC.
+% NVKF WAD IQC software is a framework for automatic analysis of DICOM objects.
+% 
+% Copyright 2012-2013  Joost Kuijer / jpa.kuijer@vumc.nl
+% 
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% ------------------------------------------------------------------------
+
+function [magnitude, phase] = WAD_MR_B0_readToshibaAAS( i_iSeries, sSeries, sParams )
+% Import function for BO uniformity Siemens phase difference field map 
+% (part of the fMRI product package). Acquisition must be single slice.
+%
+% This is a import function to be called from WAD_MR_B0_uniformity()
+%
+% Configuration name for the <params><type> field:
+% SiemensPhaseDifference
+%
+% Known limitations:
+% - Needs Explicit DICOM files. TODO: check type of field, if uint8 convert
+%   it to whatever is expected.
+% ------------------------------------------------------------------------
+% WAD MR
+% file: WAD_MR_B0_uniformity_SiemensPhaseDifference
+% 
+% ------------------------------------------------------------------------
+% VUmc, Amsterdam, NL / Joost Kuijer / jpa.kuijer@vumc.nl
+% 2009-12-22 / JK
+% first version implemented from Eline Verwer's code.
+% ------------------------------------------------------------------------
+% 2010-04-20 / JK
+% V0.94: added support for Philips double-echo GRE
+% ------------------------------------------------------------------------
+% 2012-08-13 / JK
+% V0.95
+% - adapted to WAD framework
+% - import of phase images in separate "import" function, configurable
+%   through the <type> parameter. The actual function name gets a prefix
+%   "WAD_MR_B0_read".
+% ------------------------------------------------------------------------
+% 20131127 / JK
+% V1.1
+% - new (v1.1) style action limits
+% ------------------------------------------------------------------------
+% 20140116 / JK
+% V1.1
+% - read Toshiba B0 map (product shim), adapted from readSiemens...
+% ------------------------------------------------------------------------
+
+
+% ----------------------
+% GLOBALS
+% ----------------------
+global WAD
+
+% version info
+my.name = 'WAD_MR_B0_readToshibaAAS';
+my.version = '1.1';
+my.date = '20140116';
+WAD_vbprint( ['Module ' my.name ' Version ' my.version ' (' my.date ')'] );
+
+% output arguments
+magnitude = [];
+phase = [];
+
+% default image number to be analysed
+defaultImageNumber = 9;
+
+% ----------------------------------------------------
+% check image type, get dicom header of first file
+% ----------------------------------------------------
+fname = sSeries.instance( 1 ).filename;
+WAD_vbprint( [my.name ':   Check type of B0 map... reading DICOM header of file ' fname ] );
+info = dicominfo( fname );
+
+% test for Toshiba B0 map produced from auto shim sequence, based on sequence name
+if isfield( info, 'SequenceName' ) && strcmp( info.SequenceName, 'FE_AAS' )
+    % Toshiba product shim sequence
+    WAD_vbprint( [my.name ':   Detected Toshiba iAAS.'] );
+else
+    WAD_vbprint( [my.name ':   Could not detect Toshiba B0 mao: SequenceName not equal to "FE_AAS"'] );
+    error( 'Error reading Toshiba B0 images.' )
+end
+
+
+WAD_vbprint( [my.name ':   Setting type of B0 map to Toshiba shim map.'] );
+% Toshiba stores B0 map in two series:
+% - first series has the 2 magnitude images and a phase difference image
+% - second series has the B0 map in ppm
+if info.AcquisitionNumber ~= 1
+    WAD_vbprint( [my.name ':   ERROR: B0 map [ppm] selected. Please select series with magnitude images. Skipping analysis'] );
+    error( 'Error during import of B0 map.' )
+end
+
+% -----------------------
+% find the next series...
+% -----------------------
+foundNextSeries = false;
+curStudy = WAD.in.patient(1).study(1);
+i_nSeries = length( curStudy.series );
+for ii = 1:i_nSeries
+    if ( curStudy.series( ii ).number == sSeries.number + 1 )
+        sPhsSeries = curStudy.series( ii );
+        foundNextSeries = true;
+        break;
+    end
+end
+if ~foundNextSeries
+    WAD_vbprint( [my.name ': ERROR: could not find next series for B0 image. Skip analysis.'] );
+    error( 'Error during import of phase images.' )
+end
+
+% Get image number from configuration file...
+if ~isfield( sParams, 'image' ) || isempty( sParams.image )
+    % use default image number
+    sParams.image = defaultImageNumber;
+end
+
+% magnitude series / image
+mci = sParams.image;
+% phase series / image
+pci = sParams.image;
+
+% if next series belongs to this one, it should have same name, and should
+% have one thrird of number of images.
+if ~strcmp( sSeries.description, sPhsSeries.description ) || length( sPhsSeries.instance ) ~= length( sSeries.instance ) ./ 3
+    WAD_vbprint( [my.name ':   ERROR: B0 image is in next series, but the next series doesn''t have same series description or doesn''t have matching number of images. Skip analysis'] );
+    error( 'Error during import of phase images.' )
+end
+
+% ----------------------------------------------------
+% read DICOM images
+% ----------------------------------------------------
+magnitude.filename = sSeries.instance( mci ).filename;
+magnitude.info  = dicominfo( magnitude.filename );
+magnitude.image = double( dicomread( magnitude.info ) );
+
+phase.filename  = sPhsSeries.instance( pci ).filename;
+phase.info      = dicominfo( phase.filename );
+phase.dB0_ppm   = double( dicomread( phase.info ) ) ./ 1000; % image is in ppm multiplied with factor 1000
+phase.type      = 'dB0_ppm';

commit f9389a981428722950e24762a94276943169c714

bugfix for read Toshiba B0 maps
fix problem non-Toshiba B0 maps introduced with
f0f3c2971c6dd3f81091e429545e33e48cbcba08

WAD/WAD_MR/WAD_MR_B0_uniformity.m
@@ -233,7 +233,7 @@ if isInteractive, waitbar( 0.9, h ); end
 % ----------------------------------------------------
 % bereken uniformiteit
 % ----------------------------------------------------
-matrixsize_phase = size( phase.dB0_ppm );
+matrixsize_phase = size( dB0_ppm );
 
 smallest = +1.0E99; % huge
 largest  = -1.0E99; % negative huge
@@ -241,11 +241,11 @@ largest  = -1.0E99; % negative huge
 for i=1:matrixsize_phase(1)
     for j=1:matrixsize_phase(2)
         if phasemask(i,j) == 1
-            if phase.dB0_ppm(i,j) < smallest
-                smallest = phase.dB0_ppm(i,j);
+            if dB0_ppm(i,j) < smallest
+                smallest = dB0_ppm(i,j);
             end
-            if phase.dB0_ppm(i,j) > largest
-                largest = phase.dB0_ppm(i,j);
+            if dB0_ppm(i,j) > largest
+                largest = dB0_ppm(i,j);
             end
         end
     end